d31e9ecd463dbd5306085154623eed0f28d131de
Mohammed <mbasq@Mohammed.com>
2025-05-08 00:57:20 +0300

    Update server.py for Vercel deployment

--- server.py
+++ server.py
@@ -151,6 +151,3 @@ if __name__ == '__main__':
     httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
     print(f'Starting server on port {port}...')
     httpd.serve_forever()
-
-if __name__ == '__main__':
-    run()
